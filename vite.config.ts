463e1639af2b4bddd10823680c91724838004d1b
Umakant Sharma <uksharma.forever@gmail.com>
2025-12-16 15:21:20 +0530

    configure vite base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  
-  base: '/smart-alert-insurance/',
-})
+  plugins: [react()],
+  base: "/smart-alert-insurence/",
+});
